7db074d1a46f9176e4a5a8ad739a526b587a79aa
Aji Suryawan <kazumovie132@gmail.com>
2024-02-15 08:27:10 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LatihanGit10PPLG2"
+include(":app")
+ 
